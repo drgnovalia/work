98290c21a7b6647c2f62b8be637d410e1c2508f5
setids <zetiawanindra@gmail.com>
2026-09-06 21:15:41 +0700

    Update asset paths in service worker for correct caching

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'app-nova-v1';
 const assets = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/apps.json',
-  '/manifest.json'
+  '/work/',
+  '/work/index.html',
+  '/work/style.css',
+  '/work/script.js',
+  '/work/apps.json',
+  '/work/manifest.json'
 ];
 
 // Tahap Install: Simpan semua aset utama ke memori cache
